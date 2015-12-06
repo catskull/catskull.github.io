346588e32ff4c579d22c94e1d1c633eb41829492
David DeGraw <david.degraw@gmail.com>
2015-12-05 17:06:32 -0700

    Add subtitle to gallery

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,8 +4,10 @@ var baseurl = "/public/gallery/"
 function cycle(direction) {
     image = document.getElementById('gallery_image');
     text = document.getElementById('gallery_pos_text');
+    subtitle = document.getElementById('gallery_subtitle');
+    
     if (direction) {
-      if (i == 4){
+      if (i == 12){
         i = 1;
       }
       else {
@@ -14,7 +16,7 @@ function cycle(direction) {
     }
     else {
       if (i == 1){
-        i = 4;
+        i = 12;
       }
       else {
         i = i - 1;
@@ -23,28 +25,77 @@ function cycle(direction) {
 
     switch (i) {
       case 1:
-        image.src = baseurl + '1.png'
-        text.innerHTML = "1/4";
-        console.log("case 1");
+        image.src = baseurl + '1.jpg';
+        image.alt = "Arduinoboy front";
+        text.innerHTML = "1/12";
         break;
 
       case 2:
-        image.src = baseurl + '2.png'
-        text.innerHTML = "2/4";
-        console.log("case 2");
+        image.src = baseurl + '2.jpg';
+        image.alt = "Arduinoboy back"
+        text.innerHTML = "2/12";
         break;
 
       case 3:
-        image.src = baseurl + '3.png'
-        text.innerHTML = "3/4";
-        console.log("case 3");
+        image.src = baseurl + '3.jpg';
+        image.alt = "Unassembled Arduinoboy";
+        text.innerHTML = "3/12";
         break;
 
       case 4:
-        image.src = baseurl + '4.png'
-        text.innerHTML = "4/4";
-        console.log("case 4");
+        image.src = baseurl + '4.jpg';;
+        image.alt = "Arduino clone with USB cable"
+        text.innerHTML = "4/12";
+        break;
+
+      case 5:
+        image.src = baseurl + '5.jpg';
+        image.alt = "Arduinoboy botton back";
+        text.innerHTML = "5/12";
+        break;
+
+      case 6:
+        image.src = baseurl + '6.jpg';
+        image.alt = "Arduinoboy bottom front";
+        text.innerHTML = "6/12";
+        break;
+
+      case 7:
+        image.src = baseurl + '7.jpg';
+        image.alt = "Arduinoboy right rear";
+        text.innerHTML = "7/12";
+        break;
+
+      case 8:
+        image.src = baseurl + '8.jpg';
+        image.alt = "Arduinoboy left front";
+        text.innerHTML = "8/12";
+        break;
+
+      case 9:
+        image.src = baseurl + '9.jpg';
+        image.alt = "Arduinoboy right front with screw terminals";
+        text.innerHTML = "9/12";
+        break;
+
+      case 10:
+        image.src = baseurl + '10.jpg';
+        image.alt = "Arduinoboy left front with screw terminals";
+        text.innerHTML = "10/12";
+        break;
+
+      case 11:
+        image.src = baseurl + '11.jpg';
+        image.alt = "Arduinoboy left back with screw terminals";
+        text.innerHTML = "11/12";
+        break;
+
+      case 12:
+        image.src = baseurl + '12.jpg';
+        image.alt = "Arduinoboy right back with screw terminals";
+        text.innerHTML = "12/12";
         break;
     }
-    console.log("clicked");
+
+    subtitle.innerHTML = image.alt;
 }
