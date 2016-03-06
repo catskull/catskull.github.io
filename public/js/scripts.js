05a25bd36c4025b8a0ccf1aaa594cc427f4cc9b5
David DeGraw <david.degraw@gmail.com>
2016-03-05 23:21:28 -0700

    Update images for new 1.1.1 board

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,9 +5,9 @@ function cycle(direction) {
     image = document.getElementById('gallery_image');
     text = document.getElementById('gallery_pos_text');
     subtitle = document.getElementById('gallery_subtitle');
-    
+
     if (direction) {
-      if (i == 12){
+      if (i == 8){
         i = 1;
       }
       else {
@@ -16,7 +16,7 @@ function cycle(direction) {
     }
     else {
       if (i == 1){
-        i = 12;
+        i = 8;
       }
       else {
         i = i - 1;
@@ -27,73 +27,49 @@ function cycle(direction) {
       case 1:
         image.src = baseurl + '1.jpg';
         image.alt = "Arduinoboy front";
-        text.innerHTML = "1/12";
+        text.innerHTML = "1/8";
         break;
 
       case 2:
         image.src = baseurl + '2.jpg';
         image.alt = "Arduinoboy back"
-        text.innerHTML = "2/12";
+        text.innerHTML = "2/8";
         break;
 
       case 3:
         image.src = baseurl + '3.jpg';
         image.alt = "Unassembled Arduinoboy";
-        text.innerHTML = "3/12";
+        text.innerHTML = "3/8";
         break;
 
       case 4:
         image.src = baseurl + '4.jpg';;
         image.alt = "Arduino clone with USB cable"
-        text.innerHTML = "4/12";
+        text.innerHTML = "4/8";
         break;
 
       case 5:
         image.src = baseurl + '5.jpg';
         image.alt = "Arduinoboy botton back";
-        text.innerHTML = "5/12";
+        text.innerHTML = "5/8";
         break;
 
       case 6:
         image.src = baseurl + '6.jpg';
         image.alt = "Arduinoboy bottom front";
-        text.innerHTML = "6/12";
+        text.innerHTML = "6/8";
         break;
 
       case 7:
         image.src = baseurl + '7.jpg';
         image.alt = "Arduinoboy right rear";
-        text.innerHTML = "7/12";
+        text.innerHTML = "7/8";
         break;
 
       case 8:
         image.src = baseurl + '8.jpg';
         image.alt = "Arduinoboy left front";
-        text.innerHTML = "8/12";
-        break;
-
-      case 9:
-        image.src = baseurl + '9.jpg';
-        image.alt = "Arduinoboy right front with screw terminals";
-        text.innerHTML = "9/12";
-        break;
-
-      case 10:
-        image.src = baseurl + '10.jpg';
-        image.alt = "Arduinoboy left front with screw terminals";
-        text.innerHTML = "10/12";
-        break;
-
-      case 11:
-        image.src = baseurl + '11.jpg';
-        image.alt = "Arduinoboy left back with screw terminals";
-        text.innerHTML = "11/12";
-        break;
-
-      case 12:
-        image.src = baseurl + '12.jpg';
-        image.alt = "Arduinoboy right back with screw terminals";
-        text.innerHTML = "12/12";
+        text.innerHTML = "8/8";
         break;
     }
 
